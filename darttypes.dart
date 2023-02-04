3f472ebc95ac730ecc21d2665fcf33b32e358614
santosh222222 <guravanasantosh5@gmail.com>
2023-02-04 20:12:28 +0530

    data types

---
+++ darttypes.dart
@@ -0,0 +1,10 @@
+void main() {
+  // data types
+  // numbers  int double string booleans lists maps runes symbols
+  int age = 20;
+  String name = "santosh";
+  var surname = "santosh";
+  print(age);
+
+  // all dat types in dart are objects
+}
