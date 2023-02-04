42f0f09236b2ef9d7e7306ce3bc91be3a3a424ae
santosh222222 <guravanasantosh5@gmail.com>
2023-02-04 20:15:41 +0530

    literals

---
+++ literals.dart
@@ -0,0 +1,13 @@
+void main() {
+  // litaral
+
+  var isCool = true;
+  int x = 2;
+  "jhon";
+  4.5;
+
+// string literals various ways
+  String s1 = 'single';
+  String s2 = "double";
+  String s3 = "it's easy string";
+}
