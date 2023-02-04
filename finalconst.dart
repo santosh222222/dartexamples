dd2c27a035e318738157f152ec77372fbc68f508
santosh222222 <guravanasantosh5@gmail.com>
2023-02-04 20:32:37 +0530

    finalconst

---
+++ finalconst.dart
@@ -0,0 +1,11 @@
+void main() {
+  // print(object)
+  final cityName = 'Mumabi';
+  cityName = 'delji';
+  print(cityName)
+
+  // const
+  const double PI=3.14;
+  const gravity=9.6;
+  // print()
+}
