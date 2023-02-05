6e5bc905a45761135ad3a92d6a73261c6087162f
santosh222222 <guravanasantosh5@gmail.com>
2023-02-05 08:43:57 +0530

    loops and switch

---
+++ loops.dart
@@ -0,0 +1,20 @@
+void main() {
+  // for loop
+
+  for (var i = 0; i < 4; i++) {
+    print("hello");
+  }
+  // while loop
+
+  while (i < 4) {
+    print("hello");
+    i++;
+  }
+
+// do while loop
+
+  do {
+    print("hello");
+    i++;
+  } while (i < 4);
+}
---
+++ switch.dart
@@ -0,0 +1,16 @@
+void main() {
+  String grade = '123456';
+  switch (grade) {
+    case 'A':
+      print("excelent grade");
+      break;
+
+    case 'B':
+      print("very goog !");
+      break;
+    default:
+      print("this is false");
+  }
+}
+
+// variable can only integer or string
